7eac60606e1d0d1fc11078e284742f67932a17dd
AigerimAdr <aigerim210596@gmail.com>
2023-07-29 01:06:20 +0600

    Выполинла ДЗ-5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+// UseState
+import React, {useState} from 'react'
 
-function App() {
+const LoginForm = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(name, password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={submit}>
+      <label>
+        Login:
+        <input
+          value={name}
+          onChange={event => setName(event.target.value)}
+          name="name"
+          type="text"
+        />
+      </label>
+        <label>
+        Password:
+        <input
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+          name="password"
+          type="password"
+        />
+      </label>
+          <button>Send</button>
+    </form>
   );
 }
 
-export default App;
+export default LoginForm;
+
+
+
+
+// UseContext
+// import React, { createContext, useContext } from 'react';
+
+
+// const UserContext = createContext("without provider");
+
+// const UserProvider = ({ children }) => {
+//   const user = {
+//     name: 'Eldiyar',
+//     age: 30,
+//     nationality: 'kyrgyz',
+//   };
+
+//   return (
+//     <UserContext.Provider value={user}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+
+// const UserProfile = () => {
+//   const user = useContext(UserContext);
+
+//   return (
+//     <div>
+//       <h2>User Profile</h2>
+//       <p>Name: {user.name}</p>
+//       <p>Age: {user.age}</p>
+//       <p>Nationality: {user.nationality}</p>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <UserProvider>
+//       <UserProfile />
+//     </UserProvider>
+//   );
+// };
+
+// export default App;
